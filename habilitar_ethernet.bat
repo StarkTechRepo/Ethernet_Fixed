c01283caadb1654329301a0c65a54c30743de7e1
OtaconEvil <otaconeviloficial@gmail.com>
2023-08-03 21:33:43 -0300

    Update habilitar_ethernet.bat

--- habilitar_ethernet.bat
+++ habilitar_ethernet.bat
@@ -1,4 +1,6 @@
 @echo off
+title  - https://github.com/OtaconEvil
+
 echo Habilitando el adaptador Ethernet...
 netsh interface set interface "Ethernet" admin=enabled
 echo El adaptador Ethernet ha sido habilitado exitosamente.
