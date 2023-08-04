d5c7bc3a67ff6f2a4a1cf830cc704d0095593975
OtaconEvil <otaconeviloficial@gmail.com>
2023-08-03 21:34:12 -0300

    Update resetear_ethernet.bat

--- resetear_ethernet.bat
+++ resetear_ethernet.bat
@@ -1,4 +1,6 @@
 @echo off
+title  - https://github.com/OtaconEvil
+
 echo Reiniciando la configuración del adaptador Ethernet...
 netsh interface reset interface "Ethernet"
 echo La configuración del adaptador Ethernet ha sido reiniciada exitosamente.
