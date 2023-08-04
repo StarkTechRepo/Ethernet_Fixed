b1bcbc1354342b8c8ee0a2fa267c7e9ad72d6dd6
OtaconEvil <otaconeviloficial@gmail.com>
2023-08-03 21:33:05 -0300

    Update deshabilitar_ethernet.bat

--- deshabilitar_ethernet.bat
+++ deshabilitar_ethernet.bat
@@ -1,4 +1,6 @@
 @echo off
+title  - https://github.com/OtaconEvil
+
 echo Deshabilitando el adaptador Ethernet...
 netsh interface set interface "Ethernet" admin=disabled
 echo El adaptador Ethernet ha sido deshabilitado exitosamente.
